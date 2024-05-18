988692bb0a32cc39ff08dfdc92cbbba6dced3ef6
TedrosTK <tedydada@gmail.com>
2024-05-18 18:40:11 -0400

    fetching the data

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 // API 1: "https://jsonplaceholder.typicode.com/users"
 // API 2: "https://jsonplaceholder.typicode.com/posts?userId=:id"
+
+const userListEl = document.querySelector(".user-list")
+
+async function main() {
+    const users = await fetch("https://jsonplaceholder.typicode.com/users");
+    const userData = await users.json();
+    userListEl.innerHTML = userData.map((user) => userHtml (user)).join("");
+} 
+
+function userHtml(user) {
+    return `<div class="user">
+    <div class="user-card">
+        <div class="user-card__container">
+          <h3>${user.name}</h3>
+            <p><b>Email:</b> ${user.email}</p>
+            <p><b>Phone:</b> ${user.phone}</p>
+            <p><b>Website:</b> <a href="https://${user.website}" target="_blank">${user.website}</a></p>
+        </div>
+    </div>
+</div>`
+}
+main()
