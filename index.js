0068194c92cb76187482054405ae075167ede4ef
TedrosTK <tedydada@gmail.com>
2024-05-19 17:40:14 -0400

    rerouting

--- index.js
+++ index.js
@@ -9,16 +9,20 @@ async function main() {
     userListEl.innerHTML = userData.map((user) => userHtml (user)).join("");
 } 
 
+function showUserPosts(id){
+    localStorage.setItem("id",id)
+    window.location.href = `${window.location.origin}/user.html`
+
+}
 function userHtml(user) {
-    return `<div class="user">
-    <div class="user-card">
+    return `
+    <div class="user-card" onclick="showUserPosts(${user.id})">
         <div class="user-card__container">
           <h3>${user.name}</h3>
             <p><b>Email:</b> ${user.email}</p>
             <p><b>Phone:</b> ${user.phone}</p>
             <p><b>Website:</b> <a href="https://${user.website}" target="_blank">${user.website}</a></p>
         </div>
-    </div>
-</div>`
+    </div>`
 }
 main()
