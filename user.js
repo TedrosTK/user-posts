ab80c390b20798614f7eb3f6e056eb207ea854e0
TedrosTK <tedydada@gmail.com>
2024-05-19 18:11:34 -0400

    dynammic API

--- user.js
+++ user.js
@@ -1,36 +1,26 @@
 const postListEl = document.querySelector('.post-list');
+const id = localStorage.getItem("id")
 
 async function onSearchChange(event) {
     const id = event.target.value;
-    const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
-    const postData = await posts.json();
-    
-    postListEl.innerHTML = postData.map(post => `
-    <div class="post">
-      <div class="post__title">
-        ${post.title}
-      </div>
-      <p class="post__body">
-        ${post.body}
-      </p>
-    </div>
-    `).join('');
+    renderPosts(id)
 }
-async function main() {
-    const id = localStorage.getItem("id")
-    const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
-    const postData = await posts.json();
-    
-    postListEl.innerHTML = postData.map(post => `
-    <div class="post">
-      <div class="post__title">
-        ${post.title}
-      </div>
-      <p class="post__body">
-        ${post.body}
-      </p>
-    </div>
-    `).join('');
 
+async function renderPosts(id) {
+    const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
+    const postData = await posts.json();
+    postListEl.innerHTML = postData.map(post => postHTML(post)).join('');
 }
-main()
+
+function postHTML(post) {`
+<div class="post">
+  <div class="post__title">
+    ${post.title}
+  </div>
+  <p class="post__body">
+    ${post.body}
+  </p>
+</div>
+`
+}
+renderPosts(id)
